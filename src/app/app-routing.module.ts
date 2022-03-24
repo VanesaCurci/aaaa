47b08c5c9389f95984340cbffe702e212c5cce62
Vanesa Curci <curcivanesa@gmail.com>
2022-03-23 21:30:36 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddtecnoComponent } from './add_component/addtecno/addtecno.component';
 import { ContactoComponent } from './vistas/contacto/contacto.component';
 import { InformacionComponent } from './vistas/informacion/informacion.component';
 import { InicioComponent } from './vistas/inicio/inicio.component';
@@ -7,7 +8,8 @@ import { InicioComponent } from './vistas/inicio/inicio.component';
 const routes: Routes = [
   {path:'inicio', component: InicioComponent},
   {path:'informacion', component: InformacionComponent},
-  {path:'contacto', component: ContactoComponent}
+  {path:'contacto', component: ContactoComponent},
+  {path:'addtecno', component: AddtecnoComponent}
 ];
 
 @NgModule({
--- src/app/componentes/tecnologia/tecnologia.component.ts
+++ src/app/componentes/tecnologia/tecnologia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnologia',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnologiaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+edit(){
+this.router.navigate(['addtecno'])
+}
 }
