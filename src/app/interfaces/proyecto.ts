beb5165ca0e31211cc60a0e35acc7f2f832eb541
Vanesa Curci <curcivanesa@gmail.com>
2022-03-25 21:23:02 -0300

    servicio proyecto

---
+++ src/app/interfaces/proyecto.ts
@@ -0,0 +1,5 @@
+export interface Proyecto {
+    id:number;
+    titulo:string;
+    descripcion:string;
+}
--- src/app/servicios/proyecto.service.ts
+++ src/app/servicios/proyecto.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Proyecto } from '../interfaces/proyecto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectoService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  private proyectoUrl = 'http://localhost:8080/proyectos'
+
+  getProyectos(): Observable<Proyecto[]> {
+    return this.http.get<Proyecto[]>(this.proyectoUrl)   
+  }
+
+  addProyecto(proyecto:Proyecto): Observable<Proyecto>{
+    return this.http.post<Proyecto>(this.proyectoUrl,proyecto);
+  }
+
+  deleteProyecto(id:number): Observable<Proyecto> {
+    const url = `${this.proyectoUrl}/${id}`
+    return this.http.delete<Proyecto>(url);
+  }
+
+  getProyecto(id: number): Observable<Proyecto> {
+    const url = `${this.proyectoUrl}/${id}`;
+    return this.http.get<Proyecto>(url)
+  }
+
+  updateProyecto(proyecto:Proyecto): Observable<Proyecto> {
+    const url = `${this.proyectoUrl}/${proyecto.id}?
+    titulo=${proyecto.titulo}&descripcion=${proyecto.descripcion}`;
+    return this.http.put<Proyecto>(url,proyecto) 
+  }
 }
